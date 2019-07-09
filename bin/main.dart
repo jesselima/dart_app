fa52ec0e4eb49cf9e66ece45428400aad8ed8df9
Jesse Lima <p000jesse.farias@prservicos.com.br>
2019-07-09 11:46:44 -0300

    Generics Class

--- bin/main.dart
+++ bin/main.dart
@@ -1,9 +1,14 @@
-import 'package:dart_app/someclass.dart';
+import 'package:dart_app/counter.dart';
 
 main(List<String> arguments) {
 
-  SomeClass someClass = SomeClass();
-  someClass.sayHello();
+ Counter<double> doubles = Counter<double>();
+ doubles.addAll([1.0,2.2,3.6]);
+ doubles.total();
+
+ Counter<int> ints = Counter<int>();
+ ints.addAll([1,2,3]);
+ ints.total();
 
 }
 
