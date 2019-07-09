46fc18659ec00bc2c6b8815968f8c87a8af50439
Jesse Lima <p000jesse.farias@prservicos.com.br>
2019-07-09 11:16:32 -0300

    Generics Part 2

--- bin/main.dart
+++ bin/main.dart
@@ -2,40 +2,21 @@
 
 main(List<String> arguments) {
 
-  List<int> numbers = List<int>();
-  numbers.addAll([1,2,3,4,5]);
-  print(numbers);
+  List<int> values = [1,2,3,4,5];
 
-  List<String> strings = List<String>();
-  strings.addAll(["a","b","c","d","e"]);
-  print(strings);
-
-
-    // DOES NOT WORK PROPERLY
-//  add(2, 3); // Inference
-//  add<int>(2, 3);
-//  add<double>(2.0, 3.5);
-//  add<String>("Hello", "World");
-
-  // WORKS FINE BUT NOT FOR STRINGS BECAUSE IT EXTENDS "extends num"
-  addNumbers(2, 3); // Inference
-  addNumbers<int>(2, 3);
-  addNumbers<double>(2.0, 3.5);
-  //addNumbers<String>("Hello", "World");
+  print(add(10, values));
 
 }
 
 
-// SIMPLE EXAMPLES
+T add<T extends num>(T value, List<T> items) {
 
-//void add<T>(T a, T b) {
-//  print(a + b);
-//}
+  T ret = value;
+  items.forEach((value) {
+     ret = ret + value;
+  });
+  return ret;
 
-
-// "T" will be any class that extends
-void addNumbers<T extends num>(T a, T b) {
-  print(a + b);
 }
 
 
