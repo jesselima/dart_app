ef8b4312f1b09d61cbeee04860641f06f9ee21e1
Jesse Lima <p000jesse.farias@prservicos.com.br>
2019-07-11 01:15:21 -0300

    Future - Creating and writing files in an async way.

--- bin/main.dart
+++ bin/main.dart
@@ -1,9 +1,34 @@
-import 'package:dart_app/someclass.dart';
+import 'dart:async';
+import 'dart:io';
+
 
 main(List<String> arguments) {
 
-  SomeClass someClass = SomeClass();
-  someClass.sayHello();
+  String path = Directory.current.path + "test.txt";
+  print("Appending to ${path}");
+
+  File file = File(path);
+
+  // Future is a promise
+  Future<RandomAccessFile> future = file.open(mode: FileMode.append);
+
+  future.then((RandomAccessFile raf) {
+    print("File has been opened!");
+
+    raf.writeString("Hello World")
+        .then((value) {
+          print("File have been appended");
+        })
+        .catchError((onError) => {
+          /*Do Something */
+        })
+        .whenComplete(() => {
+          raf.close(),
+          print("** The end - YES. It is.")
+        });
+  });
+
+  print("** The end - BUT NOT");
 
 }
 
