3cb73212391bf8e214a250f10066d0a1fa899405
Jesse Lima <p000jesse.farias@prservicos.com.br>
2019-07-11 00:52:42 -0300

    Timers and Callbacks with loop and defined.

--- bin/main.dart
+++ bin/main.dart
@@ -1,9 +1,48 @@
-import 'package:dart_app/someclass.dart';
+import 'dart:async';
+
+int counter = 0;
 
 main(List<String> arguments) {
 
-  SomeClass someClass = SomeClass();
-  someClass.sayHello();
+  // TESTING THE TIMER
+  print("\n\n======= TIMER");
+  Duration duration = Duration(seconds: 5);
+
+  Timer timer1 = Timer.periodic(duration, timeout /*calling a function as object*/);
+//  Timer timer2 = Timer.periodic(duration, timeoutTemp /*calling a function as object*/);
+
+  print("Started: ${getTime()}");
+
+
+
+} // CLOSES MAIN()
+
+
+// CALLBACKS - Requires: import 'dart:async';
+
+String getTime() {
+  DateTime dateTime = DateTime.now();
+  return dateTime.toString();
+}
+
+
+// Triggers forever for the given interval.
+void timeout(Timer timer) {
+  print("Timeout: ${getTime()}");
+  counter++;
+  if(counter >= 1) {
+    counter = 0;
+  }
+}
+
+// Repeats the number of repetitions, then cancel.
+int repetitions = 5;
+void timeoutTemp(Timer timer) {
+  print("Timeout: ${getTime()}");
+  counter++;
+  if(counter >= repetitions) {
+    timer.cancel();
+  }
 
 }
 
