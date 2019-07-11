3e6f944dc5414d1ec8f939644ae48e7325d261b0
Jesse Lima <p000jesse.farias@prservicos.com.br>
2019-07-11 01:30:50 -0300

    Await - Create and write to file with async await.

--- bin/main.dart
+++ bin/main.dart
@@ -1,9 +1,18 @@
-import 'package:dart_app/someclass.dart';
+import 'dart:async';
+import 'dart:io';
 
-main(List<String> arguments) {
+main(List<String> arguments) async {
 
-  SomeClass someClass = SomeClass();
-  someClass.sayHello();
+  print("starting...");
+  File file = await appendFile();
+  print("Append to file ${file.path}");
+  print("*** End.");
 
+}
+
+Future<File> appendFile() {
+  File file = File(Directory.current.path + "/test.txt");
+  DateTime dateTime = DateTime.now();
+  return file.writeAsString(dateTime.toString() + '\r\n', mode: FileMode.append);
 }
 
