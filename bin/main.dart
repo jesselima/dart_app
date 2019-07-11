fd15fcebccd8a42462cefaab799421fa3a43e73f
Jesse Lima <p000jesse.farias@prservicos.com.br>
2019-07-11 01:58:39 -0300

    GZIP vs ZLIB - Comparison.

--- bin/main.dart
+++ bin/main.dart
@@ -1,9 +1,47 @@
-import 'package:dart_app/someclass.dart';
+import 'dart:convert';
+import 'dart:io';
 
 main(List<String> arguments) {
 
-  SomeClass someClass = SomeClass();
-  someClass.sayHello();
+  int gzip = testCompression(GZIP);
+  int zlib = testCompression(ZLIB);
+
+  print("GZIP: ${gzip}");
+  print("ZLIB: ${zlib}");
+
+}
+
+String generateData() {
+  String data = "";
+
+  for(int i = 0; i < 500; i++) {
+    data = data + "Hello World\r\n";
+  }
+
+  return data;
+}
+
+int testCompression(var codec) {
+
+  String data = generateData();
+
+  // Original data
+  List original = utf8.encode(data);
+  // Compressed data
+  List compressed = codec.encode(original);
+  // Decompressed data
+  List decompress = codec.decode(compressed);
+
+  print("Original: ${original.length}");
+  print("Compressed: ${compressed.length}");
+  print("Decompressed: ${decompress.length}");
+
+  print("");
+
+  String decoded = utf8.decode(decompress);
+  assert(data == decoded);
+
+  return compressed.length;
 
 }
 
