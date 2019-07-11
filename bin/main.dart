1fa9a2049d1ad0b55d3ab56794e9aba713ac6eca
Jesse Lima <p000jesse.farias@prservicos.com.br>
2019-07-11 00:05:02 -0300

    Os Variables

--- bin/main.dart
+++ bin/main.dart
@@ -1,9 +1,31 @@
-import 'package:dart_app/someclass.dart';
+import 'dart:io';
 
 main(List<String> arguments) {
 
-  SomeClass someClass = SomeClass();
-  someClass.sayHello();
+  print("\n\n######################\nOS Variables:\n");
+  // ##################### OS Variables #########################
+  print("Os: ${Platform.operatingSystem} and version ${Platform.operatingSystemVersion}");
+
+  // TODO Info ->  Important conditional to run platform specific code when required
+  if(Platform.isWindows) {
+    print("Bleerrrr again");
+  } else if (Platform.isAndroid || Platform.isFuchsia || Platform.isIOS || Platform.isLinux || Platform.isMacOS) {
+    print("VoVoV");
+  }
+
+
+  // TODO Info ->  Show the actual pth where the current file is running.
+  print(Platform.script.path); // i.e output: /C:/android-projects/FLUTTER/dart_app/bin/main.dart
+
+  print(Platform.executable); // i.e output: C:\Dart\dart-sdk\bin\dart.exe
+
+
+  // ENVIRONMENTS VARIABLE
+  print("\n\nEnvironment variables:\n");
+  
+  Platform.environment.keys.forEach((key) => {
+    print("${key} ${Platform.environment[key]}")
+  });
 
 }
 
