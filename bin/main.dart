c7ce86e715e075b6edf6bd9163f9fb6fd659805f
jesselima <jesselima.tech@gmail.com>
2019-07-13 11:27:31 -0300

    Create a .zip file.

--- bin/main.dart
+++ bin/main.dart
@@ -1,9 +1,54 @@
-import 'package:dart_app/someclass.dart';
+import 'dart:io';
+
+import 'package:archive/archive.dart';
+import 'package:path/path.dart';
 
 main(List<String> arguments) {
 
-  SomeClass someClass = SomeClass();
-  someClass.sayHello();
+
+  // Testing...
+
+  // Create a list of files to be added to the zip file:
+  List<String> fileList = List<String>();
+  Directory.current.listSync()
+    .forEach((FileSystemEntity fileSystemEntity) {
+      if(fileSystemEntity.statSync().type == FileSystemEntityType.file) {
+        fileList.add(fileSystemEntity.path);
+      }
+    });
+
+  String zippedFilePath = 'C:\\android-projects\\FLUTTER\\dart_app\\test.zip';
+
+  zip(fileList, zippedFilePath);
+
+
+} // close main()
+
+
+
+void zip(List<String> fileList, String zippedFilePath) {
+
+  Archive archive = Archive();
+
+  fileList.forEach((String path) {
+    // Add each file
+    File currentFile = File(path);
+    ArchiveFile fileToArchive = ArchiveFile(basename(path), currentFile.lengthSync(), currentFile.readAsBytesSync()); // path, length of bytes and the bytes itself.
+    archive.addFile(fileToArchive);
+  });
+
+  ZipEncoder encoder = ZipEncoder();
+
+  File finalZippedFile = File(zippedFilePath);
+  finalZippedFile.writeAsBytesSync(encoder.encode(archive));
+
+  print("Compressed!");
+
+
 
 }
 
+
+void unzip() {
+
+}
