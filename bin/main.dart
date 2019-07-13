88b588ea26e3b998c191f5f5441aaed94942851a
jesselima <jesselima.tech@gmail.com>
2019-07-13 18:51:55 -0300

    HTTP request POST

--- bin/main.dart
+++ bin/main.dart
@@ -1,9 +1,12 @@
-import 'package:dart_app/someclass.dart';
+import 'package:http/http.dart' as http;
 
-main(List<String> arguments) {
+main(List<String> arguments) async {
 
-  SomeClass someClass = SomeClass();
-  someClass.sayHello();
+  var url = "https://www.iaaf.org/records/toplists/sprints/100-metres/outdoor/men/senior/2019";
+
+  var response = await http.get(url);
+  print("Response status code: ${response.statusCode}");
+  print("Response body: \n${response.body}");
 
 }
 
