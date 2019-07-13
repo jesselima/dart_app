93d1585fa9fbeaae0fa8cd82919abe03889b7c8d
jesselima <jesselima.tech@gmail.com>
2019-07-13 14:50:19 -0300

    Secure Random Numbers

--- bin/main.dart
+++ bin/main.dart
@@ -1,8 +1,31 @@
+import 'dart:math';
+import 'dart:typed_data';
 
+import 'package:pointycastle/pointycastle.dart';
 
 main(List<String> arguments) {
 
-
+  print(randomBytes(8));
+  print(randomBytes(16));
 
 }
 
+Uint8List randomBytes(int length) {
+
+  final rnd = SecureRandom("AES/CTR/AUTO-SEED-PRNG");
+  final key = Uint8List(16);
+  final keyParam = KeyParameter(key);
+  final params = ParametersWithIV(keyParam, Uint8List(16));
+
+  rnd.seed(params);
+
+  var random = Random();
+
+  for(int i = 0; i < random.nextInt(255); i++) {
+    rnd.nextUint8();
+  }
+  
+  var bytes = rnd.nextBytes(length);
+  return bytes;
+
+}
